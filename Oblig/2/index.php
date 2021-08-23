f097913472d98f0fda313a4bc23643d416bc90ba
proff001 <thommhole@gmail.com>
2021-08-23 10:32:57 +0200

    Added back text

--- Oblig/2/index.php
+++ Oblig/2/index.php
@@ -3,11 +3,13 @@
         <title>Oblig 2</title>
     </head>
     <body>
+        <a href="../../">Tilbake</a>
+        
         <ol>
             <li><b>Kommentarer:</b></li>
             <ul>
                 <li>Kommentarer er rett og slet tekst som ikke blir vist eller brukt av end-user, men av de som har tillgang til source koden.<br>En kommentar i html/php har en start prefix !<-- og en slutt prefix --> hvor alt innenfor prefixene er kommentaren.</li>
-                
+
             </ul>
         </ol>
     </body>
