27d992770244ee9d6fbe263a2e873d0ef369c23a
proff001 <thommhole@gmail.com>
2021-08-23 10:17:03 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Tittel???</title>
+        <link rel="stylesheet" href="./style.css">
+    </head>
+    <body>
+        <text id="title">Overskrit</text>
+        <!-- <img src="" alt=""> -->
+        
+        <!-- Litt om meg -->
+        <div>
+            <p>
+                Minim quis dolore consectetur reprehenderit consequat proident occaecat qui. 
+                Ut veniam incididunt ut ex. Veniam incididunt incididunt veniam eiusmod labore consequat amet dolor culpa. 
+                Mollit anim aute labore velit pariatur deserunt tempor voluptate nulla qui aute ipsum do dolor. 
+                Ex consectetur ad culpa deserunt quis. In pariatur pariatur ipsum ad fugiat voluptate ipsum ipsum velit aute tempor. 
+                Amet reprehenderit eu ipsum est.Sint ipsum laboris ex mollit ipsum cupidatat. 
+                Veniam tempor voluptate in ad nulla exercitation irure incididunt qui. 
+                Amet quis id culpa nulla. Ut in cillum exercitation non qui quis ea sint exercitation exercitation. 
+                Enim reprehenderit sunt cillum Lorem reprehenderit aliqua minim culpa.
+            </p>
+        </div>
+
+        <a href="./Oblig/1">Oblig 1</a>
+        <a href="https://github.com/proff001">Github</a>
+    </body>
+</html>
