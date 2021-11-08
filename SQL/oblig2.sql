5a1227980861768ed2719ff6f7cabfc3fd58f0c0
proff001 <thommhole@gmail.com>
2021-11-08 13:38:48 +0100

    SQL Oblig 2 start

--- SQL/oblig2.sql
+++ SQL/oblig2.sql
@@ -1,16 +1,35 @@
-CREATE DATABASE IF NOT EXISTS `library`;
-USE `library`;
+CREATE DATABASE IF NOT EXISTS `authorsAndBooks`;
+USE `authorsAndBooks`;
 
 CREATE TABLE IF NOT EXISTS `authors` (
-    `id` INT NOT NULL,
-    `name` VARCHAR(60) NOT NULL,
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `fornavn` VARCHAR(60),
+    `etternavn` VARCHAR(60),
+    `nasjonalitet` VARCHAR(60),
+    `adresse` VARCHAR(90),
+    `postnummer` INT,
+    `poststed` VARCHAR(60),
+    `land` VARCHAR(60),
+    `telefon` INT,
+    `epost` VARCHAR(60),
+    `sivil_status` INT,
     PRIMARY KEY (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `books` (
     `id` INT NOT NULL AUTO_INCREMENT,
-    `title` VARCHAR(60) NOT NULL,
-    `author` INT NOT NULL,
+    `tittel` VARCHAR(120),
+    `utgivelsesår` INT,
+    `ISBN` INT,
+    `forfatterID` INT,
     PRIMARY KEY (`id`),
-    FOREIGN KEY (`author`) REFERENCES `authors`(`id`)
+    FOREIGN KEY (`forfatterID`) REFERENCES `authors`(`id`)
 );
+
+INSERT INTO `authors` (`fornavn`, `etternavn`, ``, ``, ``, ``, ``, ``, ) VALUE
+    (''),
+    ('');
+
+INSERT INTO `books` (`tittel`, `utgivelsesår`, `forfatterID`) VALUE
+    (''),
+    ('');
