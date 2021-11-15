7867192c665350f419d9b0824c24d498737616e1
proff001 <thommhole@gmail.com>
2021-11-15 09:50:09 +0100

    Finished Oblig 2

--- SQL/oblig2.sql
+++ SQL/oblig2.sql
@@ -26,10 +26,33 @@ CREATE TABLE IF NOT EXISTS `books` (
     FOREIGN KEY (`forfatterID`) REFERENCES `authors`(`id`)
 );
 
-INSERT INTO `authors` (`fornavn`, `etternavn`, ``, ``, ``, ``, ``, ``, ) VALUE
-    (''),
-    ('');
+INSERT INTO `authors` (`id`, `fornavn`, `etternavn`) VALUE
+    (1, 'Aravind', 'Adiga'),
+    (2, 'David', 'Allen'),
+    (3, 'Jeff', 'Abbott'),
+    (4, 'Geir', 'Afdal'),
+    (5, 'Sharam', 'Alghasi');
 
 INSERT INTO `books` (`tittel`, `utgivelsesår`, `forfatterID`) VALUE
-    (''),
-    ('');
+    ('Mellom attentatene', 2010, 1),
+    ('Siste mann i tårnet', 2012, 1),
+    ('Hvordan få ting gjort!', 2013, 2),
+    ('Få ting gjort!', 2016, 2),
+    ('Nettverket', 2021, 3),
+    ('Undercover', 2021, 3),
+    ('Danning i barnehagen', 2013, 4),
+    ('Empirisk etikk i pedagogiske praksiser', 2014, 4),
+    ('Hvem er "vi" nå', 2020, 5),
+    ('Den globale drabantbyen', 2012, 5);
+
+SELECT CONCAT(`fornavn`, ' ', `etternavn`) AS 'Name', `epost` FROM `authors`;
+
+SELECT * FROM `books` WHERE `forfatterID` = 1;
+
+SELECT `books`.* FROM `books` INNER JOIN `authors` ON `books`.`forfatterID` = `authors`.`id` WHERE `authors`.`fornavn` = 'Sharam' AND `authors`.`etternavn` = 'Alghasi';
+
+SELECT `tittel` FROM `books`;
+
+SELECT `tittel` FROM `books` ORDER BY `tittel`;
+
+SELECT `tittel`, `ISBN` FROM `books` ORDER BY `tittel` DESC;
